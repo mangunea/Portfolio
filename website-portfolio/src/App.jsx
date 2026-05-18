c03568e6006e83163dae5f660060e0cb5a415314
mangunea <alexandjoaquin@gmail.com>
2026-05-18 10:08:38 -0400

    install tailwind css

--- website-portfolio/src/App.jsx
+++ website-portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header"
 import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 function App() {
  
   return (
     <>
     <Header></Header>
+    <Navbar></Navbar>
     <Footer></Footer>
     </>
 
