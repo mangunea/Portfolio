5d63c43d186b0e65a1a72d4b8e26b59a541c3b2a
mangunea <alexandjoaquin@gmail.com>
2026-05-18 09:52:30 -0400

    delete any unessecary files and create a base website

---
+++ website-portfolio/src/Header.jsx
@@ -0,0 +1,19 @@
+
+function Header() {
+
+    return(
+        <header>
+            <h1>Portfolio</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Projects</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+
+}
+
+export default Header
