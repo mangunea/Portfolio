654eb8340d89dc33b157c11633872a52f400f75e
mangunea <alexandjoaquin@gmail.com>
2026-05-20 10:07:17 -0400

    style using tailwind css and import roboto font

--- website-portfolio/src/Header.jsx
+++ website-portfolio/src/Header.jsx
@@ -1,12 +1,19 @@
 
+
+
 function Header() {
   return (
   <header>
-    <section>
-      <div>
-        <h1>Hi, I'm Alessandra</h1>
-        <h2>Cloud Computing Student & Problem Solver</h2>
-        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+    <section className="font-[Open Sans] min-h-screen flex items-center justify-center px-6">
+      <div className="text-center max-w-4xl">
+        <h1 className="font-[Roboto] text-4xl md:text-7xl text-[#02011a] mb-8">
+          Hi, I'm Alessandra</h1>
+
+        <h2 className="text-2xl text-gray-600 mb-12">
+          Cloud Computing Student & Problem Solver</h2>
+
+        <p className="text-1xl leading-relaxed text-black mb-14">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
       </div>
     </section>
   </header>
