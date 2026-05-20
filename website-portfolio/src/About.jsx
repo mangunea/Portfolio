b0c85f666ed407d01639bf3cf24edd50fa3ab3c0
mangunea <alexandjoaquin@gmail.com>
2026-05-20 10:12:21 -0400

    tailwind css on about me section

--- website-portfolio/src/About.jsx
+++ website-portfolio/src/About.jsx
@@ -4,10 +4,11 @@ function About() {
     return(
 
         <about>
-            <section>
-                <div>
-                    <h1>About me</h1>
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            <section className="bg-[#f5f5f5] flex items-center justify-center px-6 py-24">
+                <div className="max-w-5xl">
+                    <h2 className="text-5xl text-center mb-10">
+                        About me</h2>
+                    <p className="space-y-10 text-1xl leading-loose">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                 </div>
             </section>
         </about>
