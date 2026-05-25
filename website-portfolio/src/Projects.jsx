2e6930bb2bb5b05bfdcdf89c9614c2913a5bd764
mangunea <alexandjoaquin@gmail.com>
2026-05-25 16:00:57 -0400

    start projects section, create a 3D array for project info

--- website-portfolio/src/Projects.jsx
+++ website-portfolio/src/Projects.jsx
@@ -1,12 +1,41 @@
 
 function Projects() {
 
-    const projects = [];
+    const projects = [
+
+        { title: "Project 1 ",
+             description: "Blah blah yap yap yap yap yap taddadlooo",
+            tech: ["React", "Nani", "URMOM"], },
+        { title: "Project 2 ",
+             description: "Blah blah yap yap yap yap yap taddadlooo",
+            tech: ["React", "Nani", "URMOM"], },
+        { title: "Project 3 ",
+             description: "Blah blah yap yap yap yap yap taddadlooo",
+            tech: ["React", "Nani", "URMOM"], },
+    ];
 
     return(
 
         <section id="projects">
+            <div>
+                <h2>Featured Projects</h2>
 
+
+                <div key={index}>
+                    {projects.map((projects, index) => (
+
+                    <div>
+                        <h1>{Projects.title}</h1>
+
+
+                    </div>
+
+                    ))}
+                    
+
+                </div>
+
+            </div>
         </section>
 
     );
