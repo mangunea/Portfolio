09bfd60e4e4dbc2d96059e48774a1d58ce2b2a20
mangunea <alexandjoaquin@gmail.com>
2026-05-25 16:15:38 -0400

    create containers for each project

--- website-portfolio/src/Projects.jsx
+++ website-portfolio/src/Projects.jsx
@@ -16,25 +16,40 @@ function Projects() {
 
     return(
 
-        <section id="projects">
-            <div>
-                <h2>Featured Projects</h2>
+        <section id="projects" className="min-h-screen px-6 py-28">
+            <div className="max-w-7xl mx-auto">
 
+                <h2 className="text-5xl font-semibold text-center mb-24">
+                    Featured Projects</h2>
 
-                <div key={index}>
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
                     {projects.map((projects, index) => (
 
-                    <div>
-                        <h1>{Projects.title}</h1>
+                    <div key={index} className="border border-gray-300 rounded-2xl p-8">
+                        <h3 className="text-2xl font-semibold mb-4"
+                        >{projects.title}</h3>
 
+                        <p className="text-gray-600 text-1xl leading-relaxed mb-8">
+                        {projects.description}</p>
 
+                         <div className="flex flex-wrap gap-4 mb-10">
+                        {projects.tech.map((tech, i) => (
+                        <span key={i}
+                    className="bg-gray-200 px-4 py-2 rounded-full text-1">
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+                <button className="flex items-center gap-3 hover:underline hover: cursor-pointer text-1xl">
+                    View Project
+                </button>
+             
                     </div>
 
                     ))}
-                    
 
                 </div>
-
             </div>
         </section>
 
